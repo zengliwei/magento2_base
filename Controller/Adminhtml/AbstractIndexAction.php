5a85a4dfc6acbe5f5dda9223762e74a67e94f7b6
zengliwei <zengliwei@163.com>
2020-10-26 18:53:17 +0800

    ^ build base module

---
+++ Controller/Adminhtml/AbstractIndexAction.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Common\Base\Controller\Adminhtml;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Backend\Model\View\Result\Page;
+use Magento\Backend\Model\View\Result\Redirect;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\Request\DataPersistorInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+abstract class AbstractIndexAction extends Action implements HttpGetActionInterface
+{
+    /**
+     * @var DataPersistorInterface|mixed|null
+     */
+    protected $dataPersistor;
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    public function __construct(
+        DataPersistorInterface $dataPersistor,
+        PageFactory $resultPageFactory,
+        Context $context
+    ) {
+        parent::__construct($context);
+
+        $this->dataPersistor = $dataPersistor;
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * @param string $persistKey
+     * @param string $activeMenu
+     * @param string $pageTitle
+     * @return Page|Redirect
+     */
+    protected function parsePage(
+        string $persistKey,
+        string $activeMenu,
+        string $pageTitle
+    ) {
+        $this->dataPersistor->clear($persistKey);
+
+        /* @var $resultPage Page */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu($activeMenu);
+        $resultPage->getConfig()->getTitle()->set($pageTitle);
+
+        return $resultPage;
+    }
+}
